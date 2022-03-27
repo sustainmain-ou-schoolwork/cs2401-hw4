cf65ec5c1ba2041456f284eef2a0e198e4a0d390
Josh Marusek <jm553020@ohio.edu>
2022-03-27 19:19:05 -0400

    make previous pointer come before next pointer

--- dnode.h
+++ dnode.h
@@ -9,10 +9,10 @@ class dnode {
          * 
          * @tparam T the data type of the value to be stored by the dnode
          * @param d the value to be stored by dnode
-         * @param n the pointer to the next dnode
          * @param p the pointer to the previous dnode
+         * @param n the pointer to the next dnode
          */
-        dnode(T d = T(), dnode<T>* n = NULL, dnode<T>* p = NULL) {data_field = d; next_field = n; prev_field = p;}
+        dnode(T d = T(), dnode<T>* p = NULL, dnode<T>* n = NULL) {data_field = d; prev_field = p; next_field = n;}
 
 
         /**
@@ -23,18 +23,18 @@ class dnode {
         void set_data(T d) {data_field = d;}
 
         /**
-         * @brief Set the pointer to the next dnode.
-         * 
-         * @param n the dnode to point to
-         */
-        void set_next(dnode<T>* n) {next_field = n;}
-
-        /**
          * @brief Set the pointer to the previous dnode.
          * 
          * @param n the dnode to point to
          */
         void set_prev(dnode<T>* p) {prev_field = p;}
+
+        /**
+         * @brief Set the pointer to the next dnode.
+         * 
+         * @param n the dnode to point to
+         */
+        void set_next(dnode<T>* n) {next_field = n;}
 
 
         /**
@@ -43,19 +43,19 @@ class dnode {
         T data() const {return data_field;}
 
         /**
-         * @return the address of the next dnode
-         */
-        const dnode<T>* next() const {return next_field;}
-
-        /**
          * @return the address of the previous dnode
          */
-        const dnode<T>* prev() const {return prev_field;}
+        dnode<T>* prev() const {return prev_field;}
+
+        /**
+         * @return the address of the next dnode
+         */
+        dnode<T>* next() const {return next_field;}
 
     private:
         T data_field;          // the value stored by the dnode
-        dnode<T>* next_field;  // the pointer to the next dnode
         dnode<T>* prev_field;  // the pointer to the previous dnode
+        dnode<T>* next_field;  // the pointer to the next dnode
 };
 
 #endif
